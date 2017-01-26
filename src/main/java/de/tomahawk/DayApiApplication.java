7aef6674d5059c70642852f090b4c4bc463dbaa5
Thomas Spanner <thomas.spanner@testberichte.de>
2017-01-26 13:33:59 +0100

    adds repository, spring annotations

--- src/main/java/de/tomahawk/DayApiApplication.java
+++ src/main/java/de/tomahawk/DayApiApplication.java
@@ -1,5 +1,7 @@
 package de.tomahawk;
 
+import java.math.BigDecimal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,13 @@ public class DayApiApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hallo!");
+		
+		Product p1 = new Product();
+		p1.setName("Beard Shirt");
+		p1.setPrice(new BigDecimal(19));
+		p1.setCurrency("EUR");
+		
+		
 		SpringApplication.run(DayApiApplication.class, args);
 	}
 }
--- src/main/java/de/tomahawk/Product.java
+++ src/main/java/de/tomahawk/Product.java
@@ -2,13 +2,22 @@ package de.tomahawk;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Product {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	
 	private String name;
 	private BigDecimal price;
 	private String currency;
+	
 	
 	public long getId() {
 		return id;
